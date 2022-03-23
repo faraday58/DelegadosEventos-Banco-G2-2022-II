f5717b57276012e119efd0989f8cc967028841cd
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-03-23 12:54:18 -0600

    Excepciones

--- ATM.cs
+++ ATM.cs
@@ -21,7 +21,7 @@ namespace DelegadosEventos_Banco_G2_2022_II
                     Console.WriteLine("Por el momento no puedo realizar la operación");
                     break;
                 case 2:
-                    Console.WriteLine("1)100 \n 2) 200 \n 3) 300 \n 4) 5000 ");
+                    Console.WriteLine("1) 100 \n 2) 200 \n 3) 300 \n 4) 5000 ");
                     break;
             }
         }
@@ -30,14 +30,37 @@ namespace DelegadosEventos_Banco_G2_2022_II
         {
             Banco miBanco = new Banco();
             miBanco.ValidaUsuario += MensajeErrorValida; //Suscribiendo Método a un Evento
-
+            miBanco.Operacion += MensajeOperacion;
                                             //Método que cumple con la firma del delegado
             MensajeDelegado miDelegado = new MensajeDelegado( miBanco.Mensaje );
             Console.WriteLine("Escribe tu nombre de usuario ");
             string nombre = Console.ReadLine();
             miDelegado(nombre);
-            miBanco.DisparaValida();
 
+
+            Random funcionAleatoria = new Random();
+
+            int funciona = funcionAleatoria.Next(2);
+
+            if( funciona == 1)
+            {
+                miBanco.DisparaValida();
+            }else
+            {
+                miBanco.Operacion -= MensajeOperacion;
+                try
+                {
+                    miBanco.DisparaValida();
+                }
+                catch(NullReferenceException error)
+                {
+                    Console.WriteLine("Por el momento no cuento con efectivo");
+                }
+            }
+
+
+           
+            
 
 
 
